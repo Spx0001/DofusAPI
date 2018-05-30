7ef1e552a32a24396b9d4dc1fb4a6a35017e9dc1
q8^) <g.seins@laposte.net>
2018-05-30 17:32:59 +0200

    removed shitty code

--- src/utils/Picker.php
+++ src/utils/Picker.php
@@ -5,8 +5,29 @@ namespace dofus\utils;
 class Picker
 {
 
+    public static $config; // add static var here 
+    
+    
+    //u should learn ez code Manghao xd xd @Kittenxs
+    //i never tested this code but if it doesnt work its maybe due to syntax or u would have to tweak a little bit the code.
     public static function get($var)
     {
+        //ez as it is ! i don't like weird code written lel
+        if (!self::$config) {
+
+           // $config_file = '../src/conf/db.' . Environment::get() . '.php'; // if u want to handle project environment config u should un-comment this line
+            $config_file = '../src/conf/db.conf.php'; // if u want to handle project environment config u should un-comment this line
+            
+            if (!file_exists($config_file)) {
+                return false;
+            }
+            self::$config = require $config_file;
+        }
+
+        return self::$config[$key];
+        /*
+        
+        weird manghao code 
         $datas = array();
         $vars = explode('.', $var);
         $p = SRC . DS . 'conf' . DS . $vars[0] . '.conf.php';
@@ -19,7 +40,7 @@ class Picker
             }
         } else {
             return "V N/C";
-        }
+        }*/
     }
 
 }
